c80aadc5eeccf04ac206e124462d3cf65b2d22d1
Cameron Jenkins <cjenkins144@gmail.com>
2023-12-16 18:16:11 -0500

    Added intent update, included check and wait for new value to return

--- onosclient/intents.go
+++ onosclient/intents.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"time"
 )
 
 func (c *Client) GetIntents() (Intents, error) {
@@ -52,31 +53,82 @@ func (c *Client) GetIntent(intent Intent) (Intent, error) {
 	return resp, nil
 }
 
-func (c *Client) CreateIntent(intent Intent) error {
+func (c *Client) CreateIntent(intent Intent) (Intent, error) {
+	resp := Intent{}
 	if intent.AppID == "" || intent.Type == "" || intent.One == "" || intent.Two == "" {
-		return errors.New("invalid intent; must include AppID, Type, One, Two")
+		return resp, errors.New("invalid intent; must include AppID, Type, One, Two")
 	}
 
 	rb, err := json.Marshal(intent)
 	if err != nil {
-		return err
+		return resp, err
 	}
 
 	req, err := http.NewRequest("POST", fmt.Sprintf("%s/intents", c.HostURL), strings.NewReader(string(rb)))
 	_, err = c.doRequest(req)
 	if err != nil {
-		return err
+		return resp, err
 	}
 
-	return nil
+	resp, err = c.GetIntent(intent)
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, nil
 }
 
+func (c *Client) UpdateIntent(intent Intent) (Intent, error) {
+	//you can simply call create intent instead.
+	//as long as the key already exists, the intent will be updated
+	//this just runs a get to confirm the intent exists first in case an update only is needed.
+	//sometimes the original valuse are still returned if a get is ran very quickly after the update.
+	//It seems the values take a little time to update. may need to add some login to wait/retry
+	resp := Intent{}
+	if intent.AppID == "" || intent.Type == "" || intent.One == "" || intent.Two == "" || intent.Key == "" {
+		return resp, errors.New("invalid intent; must include AppID, Type, One, Two, Key")
+	}
+
+	_, err := c.GetIntent(intent)
+	if err != nil {
+		return resp, err
+	}
+
+	resp, err = c.CreateIntent(intent)
+	if err != nil {
+		return resp, err
+	}
+
+	//fmt.Printf("updated intent: %q; current intent: %q\n", intent.Two, resp.Two)
+	attempts := 0
+	//fmt.Println("Attempt:", attempts)
+	for resp.One != intent.One || resp.Two != intent.Two || resp.Key != intent.Key || resp.AppID != intent.AppID {
+		if attempts >= 5 {
+			break
+		}
+		//fmt.Println("Retrying:", attempts)
+		time.Sleep(100 * time.Millisecond)
+		resp, err = c.GetIntent(intent)
+		//fmt.Println("\nGot: ", resp)
+		if err != nil {
+			return resp, err
+		}
+	}
+
+	return resp, nil
+}
+
+/*
 func (c *Client) CreateIntents(intents Intents) error {
 	for _, intent := range intents.Intent {
-		fmt.Println(intent)
+		err := c.CreateIntent(intent)
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
+*/
 
 func (c *Client) DeleteIntent(intent Intent) error {
 	//this should return 200 for success and 204 for failure (no content), but onos api currently always returns 204 so there's no way to check the success/failure besides running another get and comparing.
