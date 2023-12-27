f1c4a3d3fb5b0d54558e677d4449627e78c1aecc
Cameron Jenkins <cjenkins144@gmail.com>
2023-12-27 17:41:16 -0500

    re-structure integration tests

--- run_integration_test.sh
+++ run_integration_test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 docker compose up &
 curl --request POST \
     --url http://localhost:8181/onos/v1/applications/org.onosproject.fwd/active \
@@ -8,5 +8,5 @@ curl --request POST \
     --url http://localhost:8181/onos/v1/applications/org.onosproject.openflow/active \
     --header 'Accept: application/json' \
     --header 'Authorization: Basic b25vczpyb2Nrcw=='
+docker exec -it mininet /bin/bash
 mn --topo tree,2,2 --mac --switch ovs,protocols=OpenFlow14	  --controller remote,ip=onos
-pingall
