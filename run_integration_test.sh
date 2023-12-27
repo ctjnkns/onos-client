2bde725e8faa2901563858d10874943dc8fccd17
Cameron Jenkins <cjenkins144@gmail.com>
2023-12-27 17:32:52 -0500

    add docker compose job

---
+++ run_integration_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+docker compose up &
+curl --request POST \
+    --url http://localhost:8181/onos/v1/applications/org.onosproject.fwd/active \
+    --header 'Accept: application/json' \
+    --header 'Authorization: Basic b25vczpyb2Nrcw=='
+curl --request POST \
+    --url http://localhost:8181/onos/v1/applications/org.onosproject.openflow/active \
+    --header 'Accept: application/json' \
+    --header 'Authorization: Basic b25vczpyb2Nrcw=='
+mn --topo tree,2,2 --mac --switch ovs,protocols=OpenFlow14	  --controller remote,ip=onos
+pingall
